26ffd12f7096be65bbdf7e6ca8d403b8f7d84ef6
Aapuji <79770468+Aapuji@users.noreply.github.com>
2021-03-03 09:19:32 -0500

    Added the change base function

--- js/num-base.js
+++ js/num-base.js
@@ -1,1 +1,8 @@
-
+export default function toBase(num, base = 2) {
+  let powArr = [];
+  while (num != 0) {  
+    powArr.push(num % base);
+    num = Math.floor(num / base);
+  }
+  return parseInt(powArr.reverse().join(''));
+}
